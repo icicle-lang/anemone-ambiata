8731ba9445b7f2350f738aed0b4354fb8f7fad34
Jacob Stanley <jacob@stanley.io>
2016-09-06 14:07:02 +1000

    Safer reading / writing of vints, also support for single vints

--- cbits/anemone_vint.h
+++ cbits/anemone_vint.h
@@ -6,7 +6,22 @@
 #define ANEMONE_MAX_VINT_SIZE 9
 
 //
-// Packs an array of Hadoop-style zero-compressed integers.
+// Writes a single Hadoop-style zero-compressed integer.
+//
+// x:
+//   an integer to pack
+//
+// pout:
+//   pointer to buffer large enough to hold a packed integer (ANEMONE_MAX_VINT_SIZE)
+//
+// *returns*
+//   pointer to the part of the 'pout' buffer which is unused
+//   (number of bytes used = return value - pout)
+//
+uint8_t* anemone_write_vint (int64_t x, uint8_t *pout);
+
+//
+// Writes an array of Hadoop-style zero-compressed integers.
 //
 // n:
 //   number of integers to pack
@@ -18,21 +33,39 @@
 //   pointer to buffer large enough to hold packed integers (n * ANEMONE_MAX_VINT_SIZE)
 //
 // *returns*
-//   number of bytes used to pack the integers
+//   pointer to the part of the 'pout' buffer which is unused
+//   (number of bytes used = return value - pout)
 //
-size_t anemone_pack_vint (int64_t n, const int64_t *p, uint8_t *pout);
+uint8_t* anemone_write_vint_array (int64_t n, const int64_t *p, uint8_t *pout);
 
 //
-// Unpacks an array of Hadoop-style zero-compressed integers.
+// Reads a single Hadoop-style zero-compressed integer.
 //
-// n:
-//   number of integers to unpack
-//
-// p:
-//   pointer to start of packed integers
+// pp:
+//   pointer to start of buffer
 //
 // pe:
-//   pointer to end of packed integers
+//   pointer to end of buffer
+//
+// pout:
+//   pointer to an integer for output
+//
+// *returns*
+//   zero on success, non-zero if the input was corrupt.
+//
+error_t anemone_read_vint (const uint8_t **pp, const uint8_t *pe, int64_t *pout);
+
+//
+// Reads an array of Hadoop-style zero-compressed integers.
+//
+// pp:
+//   pointer to start of buffer
+//
+// pe:
+//   pointer to end of buffer
+//
+// n:
+//   number of integers to read
 //
 // pout:
 //   pointer to buffer large enough to hold unpacked integers.
@@ -40,6 +73,6 @@ size_t anemone_pack_vint (int64_t n, const int64_t *p, uint8_t *pout);
 // *returns*
 //   zero on success, non-zero if the input was corrupt.
 //
-error_t anemone_unpack_vint (int64_t n, const uint8_t *p, const uint8_t *pe, int64_t *pout);
+error_t anemone_read_vint_array (const uint8_t **pp, const uint8_t *pe, int64_t n, int64_t *pout);
 
 #endif//__ANEMONE_VINT_H
